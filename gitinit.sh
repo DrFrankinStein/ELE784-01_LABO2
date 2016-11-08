bc1d20514b9b64a149a27c7a5e7700deb08d8ac0
DrFrankinStein <julienlemay2@gmail.com>
2016-11-08 10:38:46 -0500

    Mots de passe : timeout = 1h

--- gitinit.sh
+++ gitinit.sh
@@ -5,5 +5,6 @@ read -p "Email > " email
 
 git config --global user.name "$username"
 git config --global user.email "$email"
+git config --global credential.helper "cache --timeout=3600" 
 
 git clone https://github.com/DrFrankinStein/ELE784-01_LABO2.git
