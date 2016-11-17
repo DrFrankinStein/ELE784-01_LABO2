279fccdd8379b91ce9fe10097491f975faebf1e5
thierrydestin.er@gmail.com <thierrydestin.er@gmail.com>
2016-11-17 12:16:27 -0500

    update code point c
    update .c

--- Code/Module/Laboratoire2.c
+++ Code/Module/Laboratoire2.c
@@ -56,6 +56,12 @@ struct Camera_Dev
     struct class    *class; //
 } CamDev;
 
+struct usb_skel 
+{
+    /* One structure for each connected device */
+
+};
+
 struct file_operations ele784_fops = 
 {
     .owner    =   THIS_MODULE,
@@ -229,10 +235,61 @@ long ele784_ioctl (struct file *filp, unsigned int cmd, unsigned long arg)
 
 
 
-static int lab2_probe(struct usb_interface *interface, const struct usb_device_id *id)
+static int lab2_probe(struct usb_interface *intf, const struct usb_device_id *id)
 {
     /* called when a USB device is connected to the computer. */
 
+	struct usb_host_interface *interface;
+	struct usb_endpoint_descriptor *endpoint;
+	struct usb_device *udev = interface_to_usbdev(intf);
+	struct usb_skel *skeldev = NULL;
+
+	int i, int_end_size;
+	int retval = -ENODEV;
+
+	if (!udev)
+	{
+		printk(KERN_WARNING"udev est null");
+		return -1;
+	}
+
+	skeldev = kzalloc(sizeof(struct usb_skel), GFP_KERNEL);
+	if (!skeldev)
+	{
+		printk(KERN_WARNING"skeldev est null");
+		return -1;
+	}
+
+    //dev->command = ML_STOP;
+
+    //init_MUTEX(&dev->sem);
+    //spin_lock_init(&dev->cmd_spinlock);
+/*
+    dev->udev = udev;
+    dev->interface = interface;
+    iface_desc = interface->cur_altsetting;
+
+    // Set up interrupt endpoint information. 
+    for (i = 0; i < intf->num_altsetting; ++i)
+    {
+        endpoint = &iface_desc->endpoint[i].desc;
+
+        if (((endpoint->bEndpointAddress & USB_ENDPOINT_DIR_MASK) == USB_DIR_IN)
+                && ((endpoint->bmAttributes & USB_ENDPOINT_XFERTYPE_MASK) ==
+                    USB_ENDPOINT_XFER_INT))
+            dev->int_in_endpoint = endpoint;
+
+    }
+    if (! dev->int_in_endpoint)
+    {
+        DBG_ERR("could not find interrupt in endpoint");
+        goto error;
+    }
+
+
+    // We can register the device now, as it is ready. 
+    retval = usb_register_dev(interface, &ml_class);
+*/
 	return 0;
 }
 
@@ -339,3 +396,4 @@ module_exit(ele784_exit);
 
 // https://www.kernel.org/doc/htmldocs/writing_usb_driver/basics.html
 // http://matthias.vallentin.net/blog/2007/04/writing-a-linux-kernel-driver-for-an-unknown-usb-device/
+// http://www.makelinux.net/ldd3/chp-13-sect-4
